6095c22882c4979fe6c4eca95a83fd258ab00e7c
LemurTech <joseconde101jc@gmail.com>
2025-04-11 21:47:06 -0500

    added in some visuals

--- Project/main_project.R
+++ Project/main_project.R
@@ -1,10 +1,21 @@
-library()
-
+library(ggplot2)
 data <- read.csv("Project/synthetic_fraud_dataset.csv")
 
 head(data)
 
-describe(data)
-
 summary(data)
+
+is.null(data)
+
+barplot()
+
+#removed unnecessary columns
+df2 <- data[ , !(names(data) %in% c("Transaction_ID", "User_ID"))]
+
+barplot(df2$Transaction_Amount)
+
+ggplot(df2, aes(x=Transaction_Type))+
+  geom_bar()
+
+ggplot(df2, aes(x=Transaction_Type,y=Daily_Transaction_Count))+geom_boxplot()
 
