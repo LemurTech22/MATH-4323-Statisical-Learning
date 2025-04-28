5787b03445a9901cdbf5f6d2ed13be18e03f25e3
Jose Conde <145078308+LemurTech22@users.noreply.github.com>
2025-04-28 00:11:27 -0500

    Added additional Changes like

--- Project/main_project.R
+++ Project/main_project.R
@@ -1,21 +1,30 @@
 library(ggplot2)
 data <- read.csv("Project/synthetic_fraud_dataset.csv")
 
+print("Five elements in the dataset")
 head(data)
 
+print("Summary of Data")
 summary(data)
 
 is.null(data)
 
-barplot()
+print(paste("There is",sum(is.na(data)),"null values"))
+
+print(paste("There is",sum(duplicated(data)),"duplicated Values"))
 
 #removed unnecessary columns
 df2 <- data[ , !(names(data) %in% c("Transaction_ID", "User_ID"))]
-
-barplot(df2$Transaction_Amount)
 
 ggplot(df2, aes(x=Transaction_Type))+
   geom_bar()
 
 ggplot(df2, aes(x=Transaction_Type,y=Daily_Transaction_Count))+geom_boxplot()
+
+
+#selects the numeric columns in the dataset.
+numeric_data <- df2[sapply(df2, is.numeric)]
+
+head(numeric_data)
+
 
